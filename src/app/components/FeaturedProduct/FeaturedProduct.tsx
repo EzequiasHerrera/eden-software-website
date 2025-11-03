af42e5ec549e2ec22ba44b0cfa277758a5f9dcc4
ezequiasherrera <ezequiasherrera99@gmail.com>
2025-11-03 17:55:59 -0400

    Creating FeaturedProduct component

--- src/app/components/FeaturedProduct/FeaturedProduct.tsx
+++ src/app/components/FeaturedProduct/FeaturedProduct.tsx
@@ -1,7 +1,14 @@
 const FeaturedProduct = () => {
-  return (
-    <section id="featured-product" className="flex flex-col w-full min-h-screen justify-center items-center">FeaturedProduct</section>
-  )
-}
+    return (
+        <section
+            id="featured-product"
+            className="animate-fade-up flex flex-col w-full min-h-screen justify-center items-center">
+            <div>
+                <h2 className="text-6xl">Chatbot</h2>
+                <div className="bg-white rounded p-3"></div>
+            </div>
+        </section>
+    );
+};
 
-export default FeaturedProduct
+export default FeaturedProduct;
